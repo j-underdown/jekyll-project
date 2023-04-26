be400f77aef0e95fa4ba3348084473e21884acd0
j-underdown <62287663+j-underdown@users.noreply.github.com>
2023-04-26 09:51:01 +0200

    updated script

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,3 @@ jekyll build
 
 # Copy the contents of the _site folder to the j-underdown.github.io folder
 rsync -av --delete _site/ ../j-underdown.github.io
-
-# Commit and push changes in jekyll-project repository
-git add .
-git commit -m "Update Jekyll project files"
-git push
-
-# Commit and push changes in j-underdown.github.io repository
-(
-  cd ../j-underdown.github.io
-  git add .
-  git commit -m "Update built website files"
-  git push
-)
-
-# The working directory is not changed in this script
